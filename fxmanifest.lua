9a523c85f6e4a36f688067950069370fcf5e8eb2
Darrian <darriangaylor@icloud.com>
2022-01-04 22:59:50 -0500

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+version '1.0.0'
+
+description 'upgraded-octo-couscous'
+
+lua54 'yes'
+
+use_fxv2_oal 'yes'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
---
+++ server/sv_commands.lua
@@ -0,0 +1,6 @@
+local hidden = false
+
+RegisterCommand('test', function(source)
+    local playerId <const> = source
+    print('test')
+end, false)
