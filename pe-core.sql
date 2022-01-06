ea693a7e1233927d0200d518d0aee46867daa0c5
Christopher <chip@projecterror.dev>
2022-01-06 14:21:32 +0100

    Update pe-core.sql

--- pe-core.sql
+++ pe-core.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS `pe-core`;
 
 USE `pe-core`;
 
-CREATE TABLE `pe_characters` (
+CREATE TABLE `users` (
     `character_id` int NOT NULL AUTO_INCREMENT,
-    `user_identifier` VARCHAR(60) NOT NULL,
+    `identifier` VARCHAR(60) NOT NULL,
     `phone_number` VARCHAR(20) DEFAULT NULL;
     PRIMARY KEY (`character_id`)
 );
