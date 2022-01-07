d0ac1dc23845b09e5bf44834f18872f9a9242535
Darrian <darriangaylor@icloud.com>
2022-01-07 01:55:22 -0500

    fix: sql issue

--- pe-core.sql
+++ pe-core.sql
@@ -5,6 +5,6 @@ USE `pe-core`;
 CREATE TABLE `users` (
     `character_id` int NOT NULL AUTO_INCREMENT,
     `identifier` VARCHAR(60) NOT NULL,
-    `phone_number` VARCHAR(20) DEFAULT NULL;
+    `phone_number` VARCHAR(20) DEFAULT NULL,
     PRIMARY KEY (`character_id`)
 );
