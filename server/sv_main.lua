dfea9a8d9da8dd75bf9dce07fdeef00a0a6d8505
RockySouthpaw <55056068+RockySouthpaw@users.noreply.github.com>
2022-01-13 00:46:00 -0500

    fix: use identifier instead of char cause I’m big dumb

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -28,7 +28,7 @@ AddEventHandler('pe-core:onPlayerJoined', function()
                 Utils.Debug('inform', 'NPWD enabled. Triggering server export ^2[npwd:newPlayer]^5.')
                 exports.npwd:newPlayer({ 
                     source 		= playerId, 
-                    identifier 	= character_id,
+                    identifier 	= identifier,
                     phoneNumber     = result.phone_number
 				})
             end
@@ -42,7 +42,7 @@ AddEventHandler('pe-core:onPlayerJoined', function()
                     Utils.Debug('inform', "Inserted player into the database: {\n Identifier ["..identifier.."] \n Character ID ["..character_id.."] \n}")
                     exports.npwd:newPlayer({ 
                         source      = playerId, 
-                        identifier 	= character_id,
+                        identifier 	= identifier,
                         phoneNumber = phoneNumber
                     })
                 end)
